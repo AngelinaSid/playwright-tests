2a79c198e93233baeaff4171c30f510d39191915
angelina.sidoruk <sidoruk.ang@gmail.com>
2025-11-25 08:35:08 +0300

    правки

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 import { ICredentials } from "data/salesPortal/types/credentials.types"; 
 
 export const SALES_PORTAL_URL = process.env.SALES_PORTAL_URL!;
-export const SALES_PORTAL_API_URL = "http://localhost:8686";
+export const SALES_PORTAL_API_URL = process.env.SALES_PORTAL_API_URL!;
 
 export const credentials: ICredentials = {
   username: process.env.USER_NAME!,
